61513f37efab2eac87fa6238c260e88188a39465
adithyareddyZ <adithya.reddy@zomato.com>
2021-12-05 16:27:27 +0530

    added

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,2 +1,21 @@
-let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
-message("These files have changed: \(editedFiles.joined(separator: ", "))")
+
+import Danger
+import Foundation
+
+let danger = Danger()
+
+if danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 300 {
+    warn("Big PR, try to keep changes smaller if you can")
+}
+
+let filesToLint = (danger.git.modifiedFiles + danger.git.createdFiles).filter { !$0.contains("Documentation/") }
+
+SwiftLint.lint(inline: true)
+
+// Support running via `danger local`
+if danger.github != nil {
+    // These checks only happen on a PR
+    if danger.github.pullRequest.title.contains("WIP") {
+        warn("PR is classed as Work in Progress")
+    }
+}
